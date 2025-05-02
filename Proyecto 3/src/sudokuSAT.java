1bb6dab29c7c07d88c62c6d62a3b70ae59f0a431
f-murillo <murillo.franc@gmail.com>
2025-05-02 09:07:28 -0400

    Update sudokuSAT.java

--- Proyecto 3/src/sudokuSAT.java
+++ Proyecto 3/src/sudokuSAT.java
@@ -23,7 +23,7 @@ public class sudokuSAT{
         List<List<Integer>> clauses = new ArrayList<>();
 
         // Restricciones
-        // 1. Cada celda tiene al menos un digito.
+        // 1. Cada celda tiene al menos un digito
         for(int i = 1; i <= 9; i++){
             for(int j = 1; j <= 9; j++){
                 List<Integer> clause = new ArrayList<>();
